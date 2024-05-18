7137ec397bdc03be364a9cfa53b23371df3ad45c
DEBASHIS BARIK <158265030+Debashis143@users.noreply.github.com>
2024-05-18 05:37:41 +0000

    data types

---
+++ 1_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" ; //treat all js code as newer version
+let name ="Debashis";
+let age = 18;
+let isLoggedIn = false;
+let state;
+//number => 2 to power 53
+//bigint
+// string => ""
+//boolean => true/false
+//null => Stand alone value
+// undefined => 
+// symbol => unique
+//object =>
+    console.log(typeof "Debashis") ;
+console.log(typeof null) ;
+console.log(typeof undefined) ;   
